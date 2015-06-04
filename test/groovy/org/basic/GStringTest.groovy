eaf1d44036d2b1c036e69da284291ec275f4fd5b
cle <cle@linuxguru.fedora>
2015-06-04 16:27:10 +0200

    test Gstring

---
+++ test/groovy/org/basic/GStringTest.groovy
@@ -0,0 +1,20 @@
+package groovy.org.basic
+
+import org.codehaus.groovy.runtime.GStringImpl
+
+import static junit.framework.Assert.*
+import org.junit.Test
+
+/**
+ * Created by cle on 04/06/15.
+ */
+class GStringTest {
+    @Test
+    public void testGString(){
+        def name="John"
+        assertEquals(String.class,name.class)
+        assertEquals(String.class,"Hello".class)
+        assertTrue("Hello ${name}" instanceof GString)
+        assertEquals("Hello John","Hello ${name}".toString())
+    }
+}
