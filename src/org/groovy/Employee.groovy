3a1474ad8768286d9a88e0fd1b7c5c4ff4bb3b1d
cle <cle@linuxguru.fedora>
2015-06-04 13:08:40 +0200

    new test for getter setter functions

--- src/org/groovy/Employee.groovy
+++ src/org/groovy/Employee.groovy
@@ -1,5 +1,23 @@
 package org.groovy
 
+/**
+ * Created by cle on 04/06/15.
+ */
 class Employee {
-	String name;
+    def sureName
+    int old
+    private firstName
+
+    def getFirstName() {
+        //return is optional
+        firstName
+    }
+
+    void setFirstName(firstName) {
+        this.firstName = firstName
+    }
+
+    def getFullName() {
+        "${firstName} ${sureName}"
+    }
 }
--- test/groovy/org/basic/EmployeeTest.groovy
+++ test/groovy/org/basic/EmployeeTest.groovy
@@ -1,19 +1,26 @@
-package groovy.org.basic;
-
-import static org.junit.Assert.*
+package groovy.org.basic
 
 import org.groovy.Employee
+import org.junit.Assert
 import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertTrue
 
 class EmployeeTest {
 
-	/**
-	 *  Employee doesn't have to have setter and getter method!
-	 */
-	@Test
-	public void testGetterSetter() {
-		Employee emp=new Employee();
-		emp.setName("test");
-		assertEquals("test",emp.getName());
-	}
+    /**
+     *  Employee doesn't have to have setter and getter method!
+     */
+    @Test
+    public void testGetterSetter() {
+        Employee emp = new Employee()
+        emp.setSureName("test")
+        emp.old = 20
+        emp.setFirstName('firstname')
+        assertEquals("test", emp.getSureName())
+        assertEquals(20, emp.getOld())
+        assertEquals('firstname', emp.getFirstName())
+        assertEquals('firstname test',emp.getFullName().toString())
+    }
 }
