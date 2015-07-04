9c8c33dda91f125a1dfaba57bf2f0c536d5f7639
cle <cle@linuxguru.fedora>
2015-07-04 23:25:09 +0200

    search each function

--- test/org/groovy/file/SearchingFileTest.groovy
+++ test/org/groovy/file/SearchingFileTest.groovy
@@ -1,20 +1,44 @@
 package org.groovy.file
 
+import groovy.io.FileType
 import org.junit.Test
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+import static org.junit.Assert.assertTrue
 
 /**
  * Created by cle on 21/06/15.
  */
 class SearchingFileTest {
     @Test
-    public void testSearchGroovyfile(){
-        def bind=new Binding()
-        bind.setProperty('dir','/home/cle/work/workspace/TDDGroovy')
-        def shell=new GroovyShell(bind)
-        def result=shell.evaluate(this.class.getResource('/org/groovy/file/SearchingGroovyFiles.groovy').text) as List
-        assert result !=null
-        assert result.contains(this.class.simpleName+'.groovy') == true
+    public void testSearchGroovyfile() {
+        def bind = new Binding()
+        bind.setProperty('dir', '/home/cle/work/workspace/TDDGroovy')
+        def shell = new GroovyShell(bind)
+        def result = shell.evaluate(this.class.getResource('/org/groovy/file/SearchingGroovyFiles.groovy').text) as List
+        assert result != null
+        assert result.contains(this.class.simpleName + '.groovy') == true
         assert result.contains('SearchingGroovyFiles.groovy') == true
+    }
+
+
+    @Test
+    public void testEachfile() {
+        def testfiles=[]
+        Path path = Paths.get(this.class.getResource("/resources").toURI())
+        def rootTestResource = path.toFile()
+        assert rootTestResource.exists() == true
+        rootTestResource.eachFile(FileType.FILES){ file -> testfiles << file.getName()}
+        assert testfiles.isEmpty() == false
+        assertTrue (testfiles.contains("testfile.txt"))
+        assertTrue (testfiles.contains("testfile2"))
+
+        def subDirs=[]
+        rootTestResource.eachFile(FileType.DIRECTORIES){ file -> subDirs << file.getName()}
+        assert subDirs.isEmpty() == false
+        assertTrue (subDirs.contains("xml"))
     }
 
 }
